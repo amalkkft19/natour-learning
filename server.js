54a82292605827515cb57e24677c1ce909ee81ec
amalkkft19 <amalkkft19@gmail.com>
2023-03-16 09:35:50 +0530

    feat(server): mogodb local connected

--- server.js
+++ server.js
@@ -1,7 +1,55 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
 const config = require("./.env");
+const db = process.env.DATABASE;
+
+mongoose
+.connect(db, {
+    useCreateIndex: true,
+    keepAlive: 1,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+})
+.then((docs) => {
+    console.log(`========= DB Connected ========`);
+})
+.catch((err) => {
+    console.log(`err`, err);
+});
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:[true , "A tour name is required"],
+        unique: true,
+    },
+    description: String,
+    price: Number,
+    rating:{
+        type: Number,
+        required: [true, "A raating should be added"]
+    }
+
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+    name: "Eccentrincs Tour",
+    description: "2015-19 ec rit batch all india tour",
+    price:13000,
+    rating:4.5,
+})
+
+testTour.save().then((doc)=>{
+    console.log(doc);
+}).catch((err)=>{
+    console.log(err);
+})
+
 
 console.log("this isport",process.env.PORT);
 
